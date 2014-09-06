5383c144f37546e60ca9cbe31323a52d1babb16a
drkameleon <drkameleon@DrKameleons-MacBook-Pro.local>
2014-09-06 12:27:29 +0300

    App startup

--- app/app/AppDelegate.h
+++ app/app/AppDelegate.h
@@ -10,9 +10,11 @@
  ****************************************************/
 
 #import <Cocoa/Cocoa.h>
+#import "Web2OSX.h"
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet Web2OSX *web2OSX;
 
 @end
--- app/app/Web2OSX.m
+++ app/app/Web2OSX.m
@@ -9,8 +9,38 @@
  * Web2OSX.m
  ****************************************************/
 
+//=====================
+// Defines
+//=====================
+
+#define _RESOURCES_PATH     [[NSBundle mainBundle] resourcePath]
+#define _BASE_PATH          RSRC_PATH(@"index.html")
+
+//=====================
+// Macros
+//=====================
+
+#define RSRC_PATH(X)        [_RESOURCES_PATH stringByAppendingPathComponent:(X)]
+#define RSRC_URL(X)         [NSURL fileURLWithPath:(X)]
+
+#define STR_FROM_FILE(X)    [NSString stringWithContentsOfFile:(X) encoding:NSUTF8StringEncoding error:nil]
+
+//=====================
+// Imports
+//=====================
+
 #import "Web2OSX.h"
 
 @implementation Web2OSX
+
+//=====================
+// Start up
+//=====================
+
+- (void)awakeFromNib
+{
+    [[self mainFrame] loadHTMLString:STR_FROM_FILE(_BASE_PATH)
+                             baseURL:RSRC_URL(_BASE_PATH)];
+}
 
 @end
